149ef69caa79f5ba4eca06344cb9d40bf019cf7d
Skyler Giannetti <sjg5901@psu.edu>
2023-08-01 00:20:02 +0000

    Combine arrays challenge complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+
+import javafx.scene.control.ListCell;
+
+import java.util.ArrayList;
 
 public class CombineArrays {
     /**
@@ -9,6 +14,24 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+
+        int[] ans = new int[arr1.length + arr2.length];
+
+        // adds entirety of arr2 to end of arr1
+
+        // could create helper function to abstract this information
+        // could also use System.arraycopy()
+
+        for (int i = 0; i < arr1.length; i++) { ans[i] = arr1[i]; }         // fill ans with contents of arr1
+
+        int j = 0;
+        for (int i = arr1.length; i < ans.length; i++) {                    // starting from end of last loop, fill ans with contents of arr2
+            ans[i] = arr2[j]; 
+            j++; 
+        }
+
+        return ans;
+
+        
     }
 }
